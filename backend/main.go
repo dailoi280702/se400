2f2b0c8a605efc58bbde4a1eef8662764fd472f3
Lloyd <dailoi280702@gmail.com>
2024-07-04 16:56:18 +0700

    set up senty for backend

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"backend/handlers"
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	"github.com/getsentry/sentry-go"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	// Load env
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error loading .env file: %s\n", err.Error())
+	}
+
+	port := os.Getenv("SERVER_PORT")
+	sentryDsn := os.Getenv("SENTRY_DSN")
+
+	// Set up Sentry
+	err = sentry.Init(sentry.ClientOptions{
+		Dsn:   sentryDsn,
+		Debug: true,
+	})
+	if err != nil {
+		fmt.Printf("error initializing sentry %s", err.Error())
+	}
+	defer sentry.Flush(2 * time.Second)
+
+	// Set up server
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Pre(middleware.RemoveTrailingSlash())
@@ -25,8 +48,9 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 	go func() {
-		if err := e.Start(":8080"); err != nil && err != http.ErrServerClosed {
-			e.Logger.Fatal("shutting down the server")
+		fmt.Printf("Starting server on port %s\n", port)
+		if err := e.Start(":" + port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatalf("shutting down the server: %s", err.Error())
 		}
 	}()
 
